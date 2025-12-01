87b7cb38c3cced0984f6294879aa556ea6fe194e
Markus Wutzler <markus.wutzler@tu-dresden.de>
2025-12-01 15:55:50 +0100

    (autoconfig) dynamically rely on topology information; add options

--- platform/utils/autoconfiguration/configure_as.sh
+++ platform/utils/autoconfiguration/configure_as.sh
@@ -1,44 +1,90 @@
+#!/bin/bash
+
 # This script is used to configure ASes that were originally not preconfigured (NoConfig flag)
-# The Layer2 network is not configured, only the L3 network as well as the hosts
+# It configures only the L3 network and hosts (not Layer2)
 
-### TO UPDATE ###
-# this variable is the absolute path to the platform directory.
-PLATFORM_DIR=/home/ta/mini_internet_project/platform
-# this variable includes all the AS number that need to be configured.
-ASN_TO_CONFIGURE="3 4 13 14"
-# this variable contains all the router names that need to be configured.
-# The order is important, as it will be used to assign IP addresses to the routers.
-#ROUTER_NAMES="BIRM FRAN MUNI ZURI LYON MILA BARC NAPL"
-ROUTER_NAMES="ZURI BASE GENE LUGA MUNI LYON VIEN MILA"
-CONFIG_FILES="conf_init.sh conf_full.sh conf_rpki.sh"
+#########################################
+### VALIDATE PLATFORM DIRECTORY CONTEXT
+#########################################
+PLATFORM_DIR=$(pwd)
+if [[ $(basename "$PLATFORM_DIR") != "platform" ]]; then
+    echo "Error: This script must be run from the 'platform' directory."
+    exit 1
+fi
 
-echo "Updating configs."
+#########################################
+### DEFAULT CONFIG AND FLAG PARSING
+#########################################
+RPKI_ENABLED=true
+ASN_TO_CONFIGURE=()
+
+for arg in "$@"; do
+    case "$arg" in
+        --rpki)
+            RPKI_ENABLED=true
+            ;;
+        --no-rpki)
+            RPKI_ENABLED=false
+            ;;
+        *)
+            ASN_TO_CONFIGURE+=("$arg")
+            ;;
+    esac
+done
+
+if [[ ${#ASN_TO_CONFIGURE[@]} -eq 0 ]]; then
+    echo "Usage: $0 [--no-rpki] <ASN1> [ASN2 ASN3 ...]"
+    exit 1
+fi
+
+if $RPKI_ENABLED; then
+    CONFIG_FILES=("conf_init.sh" "conf_full.sh" "conf_rpki.sh")
+else
+    CONFIG_FILES=("conf_init.sh" "conf_full.sh")
+fi
+
+#########################################
+### LOAD ROUTER NAMES FROM FILE
+#########################################
+WORKDIR="$PLATFORM_DIR"
+ROUTER_FILE="$WORKDIR/config/l3_routers.txt"
+if [[ ! -f "$ROUTER_FILE" ]]; then
+    echo "Error: $ROUTER_FILE not found."
+    exit 1
+fi
+
+routers=($(awk '{print $1}' "$ROUTER_FILE"))
+if [[ ${#routers[@]} -eq 0 ]]; then
+    echo -e "Error: Unable to find router names in l3_routers.txt\n"
+    exit 1
+fi
+
+#########################################
+### BEGIN CONFIGURATION
+#########################################
+echo "Updating configs with RPKI: $RPKI_ENABLED"
 echo "You can ignore 'route-map' does not exist error (it will be defined)"
 echo "You can ignore 'clear ip ospf process' message (the script does that)"
 
-for group_number in $ASN_TO_CONFIGURE
-do
+for group_number in "${ASN_TO_CONFIGURE[@]}"; do
     rid=1
-    # This loop should iterate over the router, starting from lower ID to higher ID.
-    for router_name in $ROUTER_NAMES
-    do
+    for router_name in "${routers[@]}"; do
         echo "$group_number $router_name: Configuring"
         config_dir="$PLATFORM_DIR/groups/g${group_number}/${router_name}/config"
-        for config_file in $CONFIG_FILES ; do
+
+        for config_file in "${CONFIG_FILES[@]}"; do
             config_file_full="${config_dir}/${config_file}"
             chmod 755 "${config_file_full}"
             docker cp "${config_file_full}" "${group_number}_${router_name}router":"/home/${config_file}" > /dev/null
             docker exec -it "${group_number}_${router_name}router" "./home/${config_file}"
         done
 
-        docker exec -it ${group_number}_${router_name}host ip address add ${group_number}.$((100+$rid)).0.1/24 dev ${router_name}router
-        docker exec -it ${group_number}_${router_name}host ip route add default via ${group_number}.$((100+$rid)).0.2
-
+        docker exec -it ${group_number}_${router_name}host ip address add ${group_number}.$((100 + rid)).0.1/24 dev ${router_name}router
+        docker exec -it ${group_number}_${router_name}host ip route add default via ${group_number}.$((100 + rid)).0.2
 
         echo "$group_number $router_name: Clearing BGP and OSPF"
         docker exec -it "${group_number}_${router_name}router" vtysh -c 'clear ip bgp *' -c 'clear ip ospf process'
 
-
-        rid=$(($rid+1))
+        rid=$((rid + 1))
     done
 done
